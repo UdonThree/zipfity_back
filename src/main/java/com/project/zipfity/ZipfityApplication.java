8e91ed85251c4803fc64264d9d6086084648efdd
daamont <daamont01@gmail.com>
2025-12-03 14:12:37 +0900

    init project

---
+++ src/main/java/com/project/zipfity/ZipfityApplication.java
@@ -0,0 +1,13 @@
+package com.project.zipfity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipfityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipfityApplication.class, args);
+	}
+
+}
